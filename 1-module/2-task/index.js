100da9d6aee612599d6ce0cf1e477eb4fb953fd6
Filatov_Yurii <filatov_yd@mail.ru>
2020-03-09 19:30:55 +0300

    Валидация имени

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,6 +10,11 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
+  if (name === undefined || name===null || name.includes(' ') || name.length < 4) {
+    return false;
+  } else {
+    return true;
+  }
 }
 
 function sayHello() {
