599631c1eb515dfe83961ff19aeb30cd948d4ace
Filatov_Yurii <filatov_yd@mail.ru>
2020-03-10 18:43:36 +0300

    Spam test

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -4,5 +4,10 @@
  * @returns {boolean}
  */
 function checkSpam(str) {
-
+    str=str.toUpperCase();
+    if (str.includes('1XBET') || str.includes('XXX')) {return true;
+    }
+    else return false;
 }
+
+
