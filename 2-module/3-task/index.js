4fa54d89e955e670f521df0621b1dc4358ea78d8
Filatov_Yurii <filatov_yd@mail.ru>
2020-03-16 19:21:54 +0300

    calculator

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,22 @@
 let calculator = {
-  // ваш код
+  num1:null,
+  num2:null,
+
+  read(a,b){
+    this.num1=a;
+    this.num2=b;
+  },
+  sum(){
+    let summ=0;
+    summ=this.num1+this.num2;
+    return summ;
+
+  },
+  mul(){
+    let mull=0;
+    mull=this.num1*this.num2;
+    return mull;
+  }
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
